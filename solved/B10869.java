9989d780c8b6203186cebde7fbd197405f4dc0cf
songmin9813 <mithan1@naver.com>
2020-09-16 16:07:37 +0900

    solved code using java

---
+++ solved/B10869.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class B10869 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int x;
+		int y;
+		x=input.nextInt();
+		y=input.nextInt();
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y);
+		System.out.println(x%y);
+	}
+}
