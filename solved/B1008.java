83f7968fbc537c72fd48b443c497bfe078431dc5
songmin9813 <mithan1@naver.com>
2020-09-16 16:05:21 +0900

    solved code using java

---
+++ solved/B1008.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class B1008 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		double x;
+		double y;
+		x=input.nextInt();
+		y=input.nextInt();
+		double sum=x/y;
+		System.out.println(sum);
+	}
+}
