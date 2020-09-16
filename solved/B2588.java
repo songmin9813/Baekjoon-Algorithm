44311eae279d3531c935d38e1142a1d10e3fb4d3
songmin9813 <mithan1@naver.com>
2020-09-16 16:18:09 +0900

    solved code using java

---
+++ solved/B2588.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class B2588 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int x;
+		int y;
+		x=input.nextInt();
+		y=input.nextInt();
+		System.out.println(x*(y%10));
+		y/=10;
+		System.out.println(x*(y%10));
+		y/=10;
+		System.out.println(x*(y%10));
+	}
+
+}
