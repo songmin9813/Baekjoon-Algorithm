738dbb3ae436643f5913fc46d91d7d8cbef9c994
songmin9813 <mithan1@naver.com>
2020-09-16 16:00:37 +0900

    solved code using java

---
+++ solved/B10998.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class B10998 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int x;
+		int y;
+		x=input.nextInt();
+		y=input.nextInt();
+		System.out.println(x*y);
+	}
+
+}
