a08a551eef4797194abf8820881bdf3129ddfc5a
songmin9813 <mithan1@naver.com>
2020-09-17 13:51:44 +0900

    solved code using java

---
+++ solved/B2739.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class B2739 {
+
+	public static void main(String[] args) {
+		int x;
+		Scanner input = new Scanner(System.in);
+		x=input.nextInt();
+		for(int i=1;i<=9;i++)
+			System.out.println(x+" * "+i+" = "+x*i);
+	}
+}
