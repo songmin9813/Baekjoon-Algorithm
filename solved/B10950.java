a0b1e5d2856daa35fda284592d0622ec86a7fef3
songmin9813 <mithan1@naver.com>
2020-09-17 13:55:33 +0900

    solved code using java

---
+++ solved/B10950.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class B10950 {
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		int x,y,z;
+		x=input.nextInt();
+		for(int i=0;i<x;i++) {
+			y=input.nextInt();
+			z=input.nextInt();
+			System.out.println(y+z);
+		}
+	}
+}
