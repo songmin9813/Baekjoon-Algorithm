d7cd2eab97718a2a464474e2afbbafbcc40ed30a
songmin9813 <mithan1@naver.com>
2020-09-17 13:58:06 +0900

    solved code using java

---
+++ solved/B8393.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class B8393 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int x;
+		int sum=0;
+		x=input.nextInt();
+		for(int i=1;i<=x;i++)
+			sum+=i;
+		System.out.print(sum);
+	}
+}
