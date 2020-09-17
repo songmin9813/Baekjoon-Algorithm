70849407def7a7078e734fd7f9a097b3be432e85
songmin9813 <mithan1@naver.com>
2020-09-17 13:49:06 +0900

    solved code using java

---
+++ solved/B2884.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class B2884 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int x, y;
+		x=input.nextInt();
+		y=input.nextInt();
+		if(y<45) {
+			y=60-Math.abs(y-45);
+			if(x==0) x=23;
+			else x-=1;
+		}
+		else y-=45;
+		System.out.println(x+" "+y);
+	}
+}
