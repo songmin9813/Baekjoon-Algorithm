eeb299e7104e43e5f2e5a07d28b9b3f989385428
songmin9813 <mithan1@naver.com>
2020-09-18 21:27:26 +0900

    solved code using java

---
+++ solved/B11021.java
@@ -0,0 +1,25 @@
+import java.io.*;
+public class B11021 {
+	public static void main(String[] args) {
+		int sum;
+		try {
+			BufferedReader input=new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter output=new BufferedWriter(new OutputStreamWriter(System.out));
+			int x=Integer.parseInt(input.readLine());
+			
+			for(int i=0;i<x;i++) {
+				String s=input.readLine();
+				String[] array=s.split(" ");
+				sum=0;
+				for(int i1=0;i1<array.length;i1++)
+					sum+=Integer.parseInt(array[i1]);
+				output.write("Case #"+(i+1)+": "+String.valueOf(sum+"\n"));
+			}
+			output.flush();
+			output.close();
+			input.close();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
