16d945f7c678f911068d1ca86adea492b426fb5f
songmin9813 <mithan1@naver.com>
2020-09-18 21:17:59 +0900

    solved code using java:

---
+++ solved/B2742.java
@@ -0,0 +1,18 @@
+import java.io.*;
+public class B2742 {
+	public static void main(String[] args) {
+		int sum;
+		try {
+			BufferedReader input=new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter output=new BufferedWriter(new OutputStreamWriter(System.out));
+			int x=Integer.parseInt(input.readLine());
+			for(int i=x;i>0;i--) 
+				output.write(i+"\n");
+			output.flush();
+			output.close();
+			input.close();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
