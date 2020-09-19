8a265701c0f84551e6a6bb1adca7dcf95f98e2a2
songmin9813 <mithan1@naver.com>
2020-09-19 14:30:45 +0900

    solved code using java

---
+++ solved/B2438.java
@@ -0,0 +1,20 @@
+import java.io.*;
+public class B2438 {
+	public static void main(String[] args) {
+		int sum;
+		try {
+			BufferedReader input=new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter output=new BufferedWriter(new OutputStreamWriter(System.out));
+			int x=Integer.parseInt(input.readLine());
+			for(int i=1;i<=x;i++) {
+				for(int i1=1;i1<=i;i1++) output.write("*");
+				output.write("\n");
+			}
+			output.flush();
+			input.close();
+			output.close();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
