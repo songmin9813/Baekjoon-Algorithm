4876413bce9651aed1df2cbb6a092ed5ab2ddc63
songmin9813 <mithan1@naver.com>
2021-12-27 18:05:33 +0900

    BOJ14500 보류

--- solved/14500.py
+++ solved/14500.py
@@ -1,16 +1,6 @@
 import sys
 def checking(board,x,y):
     global result,n,m
-    blueX=[[x,x,x,x],[x,x+1,x+2,x+3]]
-    blueY=[[y,y+1,y+2,y+3],[y,y,y,y]]#일자모양좌표
-    yellowX=[[x,x,x+1,x+1]]
-    yellowY=[[y,y+1,y,y+1]]#네모모양좌표
-    orangeX=[[x,x,x-1,x-2],[x,x-1,x-1,x-1],[x,x,x+1,x+2],[x,x+1,x+1,x+1],[x,x,x-1,x-2],[x,x+1,x+1,x+1],[x,x,x+1,x+2],[x,x-1,x-1,x-1]]
-    orangeY=[[y,y-1,y-1,y-1],[y,y,y+1,y+1],[y,y+1,y+1,y+1],[y,y,y-1,y-2],[y,y+1,y+1,y+1],[y,y,y+1,y+1],[y,y-1,y-1,y-1],[y,y,y-1,y-2]]#이상한니은좌표
-    greenX=[[x,x+1,x+1,x+2],[x,x,x-1,x-1],[x,x+1,x+1,x+2],[x,x,x-1,x-1]]
-    greenY=[[y,y,y+1,y+1],[y,y+1,y+1,y+2],[y,y,y-1,y-1],[y,y-1,y-1,y-2]]#초록색
-    redX=[[x,x,x,x+1],[x,x+1,x+1,x+2],[x,x,x-1,x],[x,x+1,x+1,x+2]]
-    redY=[[y,y+1,y+2,y+1],[y,y,y-1,y],[y,y+1,y+1,y+2],[y,y,y+1,y]]#법규
     listX=[[x,x,x,x],[x,x+1,x+2,x+3],[x,x,x+1,x+1],[x,x,x-1,x-2],[x,x-1,x-1,x-1],[x,x,x+1,x+2],[x,x+1,x+1,x+1],[x,x,x-1,x-2],[x,x+1,x+1,x+1],[x,x,x+1,x+2],[x,x-1,x-1,x-1],
     [x,x+1,x+1,x+2],[x,x,x-1,x-1],[x,x+1,x+1,x+2],[x,x,x-1,x-1],[x,x,x,x+1],[x,x+1,x+1,x+2],[x,x,x-1,x],[x,x+1,x+1,x+2]]
     listY=[[y,y+1,y+2,y+3],[y,y,y,y],[y,y+1,y,y+1],[y,y-1,y-1,y-1],[y,y,y+1,y+2],[y,y+1,y+1,y+1],[y,y,y-1,y-2],[y,y+1,y+1,y+1],[y,y,y+1,y+2],[y,y-1,y-1,y-1],[y,y,y-1,y-2],
