12902c67bc52019cd923fadfae13b5c2869f968d
songmin9813 <mithan1@naver.com>
2022-01-15 23:49:32 +0900

    solved BOJ11504 주석추가

--- solved/11504.py
+++ solved/11504.py
@@ -5,14 +5,14 @@ board=list(map(int,sys.stdin.readline().split()))
 dp=[]
 for i1,v1 in enumerate(board):
     if i1==0:
-        dp.append([1,1])
+        dp.append([1,1])#해당수로 올라가는 수인지, 내려가는 수인지 각각을 저장할 필요가 있었음
         continue
     up,down=-1,-1
     for i2 in range(0,i1):
         if board[i2]<board[i1]:
-            up=max(up,dp[i2][0])
+            up=max(up,dp[i2][0])#올라가는 경우 올라가는 수만 확인
         elif board[i2]>board[i1]:
-            down=max(down,max(dp[i2][0],dp[i2][1]))
+            down=max(down,max(dp[i2][0],dp[i2][1]))#내려가는 경우 내려가는 경우 외에도 정점을 찍고 내려가는 경우가 생김
         else:
             pass
     if up==-1:
