eefcd54d2c8cf2c8facb923b8eecd1af34b53fe5
songmin9813 <mithan1@naver.com>
2022-03-14 17:56:59 +0900

    solved BOJ1912 간단 dp 리펙터링

--- solved/1912.py
+++ solved/1912.py
@@ -1,7 +1,6 @@
 import sys
 n=int(sys.stdin.readline())
-list_=list(map(int,sys.stdin.readline().split()))
-dp=list_.copy()
+dp=list(map(int,sys.stdin.readline().split()))
 for i in range(1,len(dp)):
     dp[i]=max(dp[i-1]+dp[i],dp[i])
 print(max(dp))
