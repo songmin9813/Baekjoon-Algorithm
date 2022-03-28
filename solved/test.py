a6a64a4181be006b6c116daca3d2666fbb767324
songmin9813 <mithan1@naver.com>
2022-03-29 00:09:52 +0900

    테스트용

--- solved/test.py
+++ solved/test.py
@@ -1,54 +1,19 @@
-from collections import deque#BFS를 사용하기 위한 import
-import copy#각 퍼즐의 상태 복사본을 만들기 위한 import
-puzzles=[['1','2','3'],['4',' ','5'],['7','8','6']]
-ans=[['1','2','3'],['4','5','6'],['7','8',' ']]
-all_nodes={'A':puzzles,'goal':ans}
-alphabet='B'
+puzzles=[['1','2','3'],['4',' ','5'],['7','8','6']]#퍼즐이 4방향 탐색이 필요한 경우
+#2차원 배열로도 개쩌는 방향 탐색 코딩을 할 수 있다.
+#인국쉑은 1차원 배열로 미친 코딩하던데 꼴보기 싫어서 난 원래 하던대로 함
+n,m=3,3#n행 m열 기준임
+start=[0,0]#처음 시작은 0행 0열
 
-def make_node(al,puzzles,open,closed):
-    dx=[-1,1,0,0]
-    dy=[0,0,-1,1]
-    #탐색의 순서는 right,down,up,left 순임을 알 수 있음
-    #다르게 할 경우 순서가 터져버리는 문제 발생
-    for i1 in range(3):
-        for i2 in range(3):
-            if puzzles[i1][i2]==' ':#이 순간부터 노드를 만들어야 함 
-                for i3 in range(4):
-                    newX=i1+dx[i3]
-                    newY=i2+dy[i3]
-                    if 0<=newX<3 and 0<=newY<3:#새로운 위치가 범위 안에 들어있는 경우
-                        new_puzzles=copy.deepcopy(puzzles)
-                        new_puzzles[i1][i2]=new_puzzles[newX][newY]
-                        new_puzzles[newX][newY]=' '
-                        #복사본 생성 후 swap
-                        existed=False
-                        for check in open:
-                            if new_puzzles==all_nodes[check]:
-                                existed=True
-                        for check in closed:
-                            if new_puzzles==all_nodes[check]:
-                                existed=True
-                                #open, closed에 이미 노드가 있는지 확인
-                        if not existed: # #open에 들어가야 하는 순간
-                            global alphabet
-                            all_nodes[alphabet]=new_puzzles
-                            open.append(alphabet)
-                            alphabet=chr(ord(alphabet)+1)
-                return#노드 생성은 빈칸 기준으로 최대 4번만 하면 됨
 
-def breadth_first_search(start):
-    open=deque()
-    closed=deque()
-    open.append(start)
-    while open:
-        current_alphabet=open.popleft()
-        if all_nodes[current_alphabet]==all_nodes['goal']:
-            print('목표 노드 발견! 생성된 노드는 '+str(len(all_nodes)-1)+'개 입니다.')
-            return True
-        closed.appendleft(current_alphabet)
-        make_node(current_alphabet,all_nodes[current_alphabet],open,closed)
-        #노드의 생성이 이루어져야 함
-        print('open='+str(list(open))+'; closed='+str(list(closed)))
-    return False
+#이 아래부터가 중요함
 
-breadth_first_search('A')
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+#4방향 탐색을 위한 각 dx dy 설정.
+#dx dy에 따라 up,down,left,right 순으로 움직이겠네
+
+for i in range(4):#4방향이니까
+    newX=start[0]+dx[i]#start 좌표 기준으로 dx에서 하나씩 꺼내옴
+    newY=start[1]+dy[i]#dy에서 하나씩 꺼내옴
+    if 0<=newX<n and 0<=newY<m:#행과 열값 안에 있음을 보장해야 함
+        print('새로운 좌표가 안에 들어왔네요 >_<')
